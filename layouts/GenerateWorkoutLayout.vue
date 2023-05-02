e0609160614025ea0050f315c5a96e6248d6cefc
Jonah Pine <jpine4@hotmail.com>
2023-05-02 17:06:53 -0400

    Google ads

--- layouts/GenerateWorkoutLayout.vue
+++ layouts/GenerateWorkoutLayout.vue
@@ -13,6 +13,13 @@
       <!-- <div class="w-full h-80 mb-10">
         <img class="w-full h-full object-cover rounded-xl shadow-xl" src="https://images.unsplash.com/photo-1580670029149-5c00eec8bb66?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
       </div> -->
+
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6268121744351804"
+        data-ad-slot="9445078662"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
       
     </div>
   
@@ -21,6 +28,18 @@
   <script setup lang="ts">
   
     import { useStore } from '../stores/useStore'
+
+    useHead({
+      script: [{
+        'data-ad-client': 'ca-pub-6268121744351804',
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+        async: true
+      },
+      {
+        children: "(adsbygoogle = window.adsbygoogle || []).push({});"
+      }
+    ]
+    })
 
     const state = useStore()
 
